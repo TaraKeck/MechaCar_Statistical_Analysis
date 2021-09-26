10c254cb2516ac2c6cca33ae94e509ea90ea4eda
TaraKeck <85530690+TaraKeck@users.noreply.github.com>
2021-09-25 22:17:12 -0400

    Add files via upload

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,36 @@
+#Deliverable 1-----------------------------------------------------------
+
+#load dplyr package
+library(dplyr)
+library(tidyverse)
+
+#read file
+MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+#perform linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar)
+
+#perform summary
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
+
+#Deliverable 2------------------------------------------------------------
+# read csv to dataframe
+suspension_coil <- read.csv('Suspension_Coil.csv', stringsAsFactors=F)
+
+# Create summary data frame
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+# Create summary data frame grouped by lot
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+
+#Deliverable 3 -----------------------------------------------------------
+#perform t-test to determine if the PSI across 
+# Peform t-test across all Lots
+t.test(Suspension$PSI,mu = 1500)
+# Peform t-test on Lot 1
+t.test(subset(Suspension,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+# Peform t-test on Lot 2
+t.test(subset(Suspension,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+# Peform t-test on Lot 3
+t.test(subset(Suspension,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
